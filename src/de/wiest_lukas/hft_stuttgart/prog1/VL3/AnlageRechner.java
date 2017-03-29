5d7b81a8177e28ccb9d04918b182987b2320a902
9Lukas5 <9Lukas5@users.noreply.github.com>
2017-03-29 08:50:41 +0200

    get a valid fund value

--- src/de/wiest_lukas/hft_stuttgart/prog1/VL3/AnlageRechner.java
+++ src/de/wiest_lukas/hft_stuttgart/prog1/VL3/AnlageRechner.java
@@ -1,5 +1,7 @@
 package de.wiest_lukas.hft_stuttgart.prog1.VL3;
 
+import java.util.Scanner;
+
 public class AnlageRechner
 {
     // global vars
@@ -9,7 +11,36 @@ public class AnlageRechner
 
     public static void main(String[] args)
     {
-        System.out.println(calculateInterest(1000, 5));
+        // vars
+        double fund = 0;
+        Scanner in = new Scanner(System.in);
+
+        getFund:
+        do
+        {
+            try
+            {
+                System.out.println("Please insert fund. Minimum: 2500 EUR");
+                fund = in.nextDouble();
+                in.nextLine();          // catch flying enter
+            }
+            catch (Exception e)
+            {
+                System.out.println("That's not a valid fund, please re-enter program");
+                continue getFund;
+            }
+
+            fund *= (double) 100;       // make the entered fund a Cent value
+
+            if (fund < MIN_EINLAGE)
+            {
+                System.out.println("you have to insert a fund higher or atleast 2500 EUR!");
+            }
+            else
+            {
+                break getFund;
+            }
+        } while (true);
     }
 
     public static double calculateInterest(double fund, double rate)
